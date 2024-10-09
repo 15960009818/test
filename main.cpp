f5edbdc5102a2d6e92c4a787f2645a6e158fc82a
xiangjun <1327171351@qq.com>
2024-10-09 15:59:12 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file           : main.cpp
+  * @author         : 1
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/10/9
+  ******************************************************************************
+  */
+
+#include "main.h"
+#include <iostream>
+using namespace
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
